10f58bdab55373bc529a5fcf04f6385d3d0ca1c1
eritry <erika.shefer@gmail.com>
2019-05-11 16:47:30 +0300

    clojure updated

--- clojure/expression.clj
+++ clojure/expression.clj
@@ -3,23 +3,52 @@
     (fn [var]
       (apply f (map (fn [x] (double (x var))) args)))))
 
+(defn min-max-operation [f]
+  (fn [& args]
+    (fn [var]
+      (reduce f (map (fn [x] (double (x var))) args)))))
+
 (def add (operation +))
 (def subtract (operation -))
 (def multiply (operation *))
 (def divide (operation (fn [a b] (/ (double a) (double b)))))
 (def negate (operation -))
-(def min (operation min))
-(def max (operation max))
 
-(defn constant [x] (fn [var] x))
+(def min (min-max-operation clojure.core/min))
+(def max (min-max-operation clojure.core/max))
+
+(defn constant [x] (constantly x))
 (defn variable [x] (fn [var] (get var x)))
 
-(defn parseFunction [expr]
+(defn parse [expr]
   (let [operations { '+ add, '- subtract, '/ divide, '* multiply, 'negate negate, 'min min, 'max max}]
     (cond
-      (seq? expr) (let [e (first expr)] (apply (get operations e) (map parseFunction (rest expr))))
-      (string? expr) (parseFunction (read-string expr))
+      (seq? expr) (let [e (first expr)] (apply (get operations e) (map parse (rest expr))))
       (number? expr) (constant expr)
-      (symbol? expr) (variable (str expr))
-      )))
+      (symbol? expr) (variable (str expr)))))
 
+(def parseFunction
+  (comp parse read-string))
+
+
+(defn broadcast [b t db dt]
+  (cond
+    (== 0 db) (vec [b t])
+    (== db dt) (vec (mapv (fn [bi ti] (broadcast bi ti (dec db) (dec dt))) b t))
+    :else (vec (mapv (fn [bi] (broadcast bi t (dec db) dt)) b))))
+
+(defn check-b2b [operation]
+  (fn
+    ([b] b)
+    ([b & bs]
+     (apply (check-b2b operation) (
+                                    (fn [b t]
+                                      (if (< (depth b) (depth t))
+                                        (unary-b2b operation (apply operation (broadcast t b (depth t) (depth b))) (depth t))
+                                        (println (apply + (broadcast b t (depth b) (depth t))))
+                                        ;(apply operation (broadcast b t (depth b) (depth t)))
+                                        )
+                                      ) b (nth bs 0)
+                                    ) (rest bs))
+      ))
+  )
--- clojure/linear.clj
+++ clojure/linear.clj
@@ -3,71 +3,69 @@
 (defn equal-len?
   ([] true)
   ([v] true)
-  ([v u] (and (is-vector? v u) (== (count u) (count v))))
+  ([v u] (== (count u) (count v)))
   ([v u & vs] (and (equal-len? v u) (apply equal-len? u vs))))
 
-(defn apply-v2v [operation] (fn [v & vs]
-                              {:pre [(apply equal-len? v vs)]}
-                              (apply mapv operation v vs)))
+(defn matrix? [m] (and (is-vector? m) (apply equal-len? m)))
+(defn is-matrix? [& args] (every? matrix? args))
 
-(defn apply-v2s [operation] (fn
-                       ([args] args)
-                       ([args & vals] (mapv (fn [f] (apply operation f vals)) args))))
+(defn apply-v2v [operation]
+  (fn [v & vs]
+    {:pre [(apply equal-len? v vs)]}
+    (apply mapv operation v vs)))
+
+(defn apply-m2m [operation]
+  (fn [m & ms]
+    {:pre [(apply equal-len? m ms) (is-matrix? (apply vector m ms))]}
+    (apply mapv operation m ms)))
+
+(defn apply-v2s [operation]
+  (fn
+    ([args] args)
+    ([args & vals] (mapv (fn [f] (apply operation f vals)) args))))
 
 (defn form [b] (loop [v (vector)
                       cur b]
                  (if
-                   (instance? Number cur)
+                   (or (instance? Number cur) (= (count cur) 0))
                    v
-                   (recur (conj v (count cur)) (cur 0))
+                   (recur (conj v (count cur)) (nth cur 0))
                    )))
 
 
-(defn depth [b] (if
-                  (instance? Number b)
-                  0
-                  (+ (depth (b 0)) 1)))
-
-
-(defn unary-b2b [operation b d] (if
-                                  (== 0 d)
-                                  (operation b)
-                                  (mapv (fn [b] (unary-b2b operation b (dec d))) b)))
-
-(defn broadcast [operation b t db dt]
-  (cond
-    (== 0 db) (operation b t)
-    (== db dt)
-    (vec (for [i (range (count b))] (broadcast operation (b i) (t i) (dec db) (dec dt))))
-    :else (vec (for [i (range (count b))] (broadcast operation (b i) t (dec db) dt)))))
-
-
-(defn check-b2b [operation]
-  (fn
-    ([b] b)
-    ([b & bs]
-     (apply (check-b2b operation) (
-                                    (fn [b t]
-                                      (if (< (depth b) (depth t))
-                                        (unary-b2b operation (broadcast operation t b (depth t) (depth b)) (depth t))
-                                        (broadcast operation b t (depth b) (depth t)))
-                                      ) b (nth bs 0)
-                                    ) (rest bs))
-      ))
+(defn broadcast [b t]
+  {:pre (= (nthrest t (- (count t) (count (form b)))) (form t))}
+  (if (equal-len? (form b) t)
+    b
+    (vec (repeat (nth t 0) (broadcast b (rest t)))))
   )
+
+(defn gen-operator [o]
+  (fn
+    ([x]
+     (if (number? x)
+       (o x)
+       (mapv (gen-operator o) x)))
+    ([v u]
+     (if (number? v)
+       (o v u)
+       (mapv (gen-operator o) v u)))))
 
 (defn apply-b2b [operation]
   (fn
-    ([b] (unary-b2b operation b (depth b)))
-    ([b & bs] (apply (check-b2b operation) b bs))))
-
+    ([b] ((gen-operator operation) b))
+    ([b t]
+     (let [m (max-key count (form b) (form t))]
+       ((gen-operator operation) (broadcast b m) (broadcast t m))))
+    ([b t & ts]
+     (reduce (apply-b2b operation) ((apply-b2b operation) b t) ts))))
 
 (def v+ (apply-v2v +))
 (def v- (apply-v2v -))
 (def v* (apply-v2v *))
-(def m+ (apply-v2v v+))
-(def m- (apply-v2v v-))
-(def m* (apply-v2v v*))
+(def m+ (apply-m2m v+))
+(def m- (apply-m2m v-))
+(def m* (apply-m2m v*))
 
 (def v*s (apply-v2s *))
 (def m*s (apply-v2s v*s))
@@ -89,7 +87,7 @@
 (defn vect
   ([v] {:pre [(vector? v)]} v)
   ([v u]
-   {:pre [(equal-len? v u) (== (count v) 3) (== (count u) 3)]}
+   {:pre [(is-vector? v u) (== (count v) 3) (== (count u) 3)]}
    (vector (- (* (v 1) (u 2)) (* (v 2) (u 1)))
            (- (* (v 2) (u 0)) (* (v 0) (u 2)))
            (- (* (v 0) (u 1)) (* (v 1) (u 0)))))
