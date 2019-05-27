58f7861b9920741c34f36c3cea152aedd73fc226
Erika <erika.shefer@gmail.com>
2019-05-27 14:51:56 +0300

    hw 9 updated

--- clojure/expression.clj
+++ clojure/expression.clj
@@ -4,7 +4,10 @@
     (fn [var]
       (apply f (map (fn [x] (double (x var))) args)))))
 
-(defn div [a b] (/ (double a) (double b)))
+(defn div [a & bs] (reduce (fn [a b] (/ (double a) (double b))) a bs))
+(defn neg [x] (- x))
+(defn sqr [x] (* x x))
+(defn sqrt [x] (Math/sqrt (Math/abs x)))
 
 (def add (operation +))
 (def subtract (operation -))
@@ -30,12 +33,15 @@
   (comp parse read-string))
 
 ;homework 9
+(declare Add)
+(declare Subtract)
+(declare Multiply)
+(declare Divide)
+
 (definterface Expression (evaluate [var]) (^String toString []) (diff [name]))
 (defn evaluate [expression var] (.evaluate (expression) var))
 (defn toString [expression] (.toString (expression)))
 (defn diff [expression name] (.diff (expression) name))
-
-(deftype Const [value])
 
 (deftype Const [value]
   Expression
@@ -43,58 +49,50 @@
   (toString [_] (format "%.1f" value))
   (diff [_ _] (constantly (Const. 0))))
 
-(def ONE (Const. 1))
-(def ZERO (Const. 0))
+(defn Constant [value] (constantly (Const. value)))
+(def TWO (Constant 2))
+(def ONE (Constant 1))
+(def ZERO (Constant 0))
 
 (deftype Var [value]
   Expression
   (evaluate [_ var] (get var value))
   (toString [_] value)
-  (diff [_ name] (if (= name value) (constantly ONE) (constantly ZERO))))
-
-(deftype Operation [op type diff-f arguments]
-  Expression
-  (evaluate [_ var] (apply op (map (fn [x] (evaluate x var)) arguments)))
-  (toString [_] (str "(" type " " (clojure.string/join " " (map (fn [x] (toString x)) arguments)) ")"))
-  (diff [_ name] (apply diff-f name arguments)))
-
-(defn build-operation [op type diff-f] (fn [& arguments] (constantly (Operation. op type diff-f arguments))))
-
-(defn get-diff [name]
-  (fn [x] (diff x name)))
-(declare diff-multiply)
-(declare diff-divide)
-(declare Sinh)
-
-(declare Cosh)
+  (diff [_ name] (if (= name value) ONE ZERO)))
 (defn Variable [value] (constantly (Var. value)))
-(defn Constant [value] (constantly (Const. value)))
-(def TWO (Constant 2))
 
-(def Add (build-operation + "+" (fn [name & args] (apply Add (map (get-diff name) args)))))
-(def Subtract (build-operation - "-" (fn [name & args] (apply Subtract (map (get-diff name) args)))))
-(def Multiply (build-operation * "*" (fn [name & args] (apply diff-multiply name args))))
-(def Divide (build-operation (fn [& args] (reduce div args)) "/" (fn [name & args] (apply diff-divide name args))))
-(def Divide (build-operation (fn [& args] (reduce div args)) "/" (fn [name & args] (apply diff-divide name args))))
-(def Negate (build-operation (fn [x] (- x)) "negate" (fn [name arg] (Negate ((get-diff name) arg)))))
-(def Sinh (build-operation (fn [x] (Math/sinh x)) "sinh" (fn [name arg] (Multiply (Cosh arg) (diff arg name)))))
-(def Cosh (build-operation (fn [x] (Math/cosh x)) "cosh" (fn [name arg] (Multiply (Sinh arg) (diff arg name)))))
-(def Square (build-operation (fn [x] (* x x)) "square" (fn [name arg] (Multiply ((get-diff name) arg) TWO arg))))
-(def Sqrt (build-operation (fn [x] (Math/sqrt (Math/abs x))) "sqrt"
-                           (fn [name arg] (Divide (Multiply arg ((get-diff name) arg))
-                                                  (Multiply TWO (Sqrt (Multiply (Square arg) arg)))))))
+(def Operations {})
+
+(deftype Operation [type arguments]
+  Expression
+  (evaluate [_ var] (apply (nth (get Operations type) 0) (map (fn [x] (evaluate x var)) arguments)))
+  (toString [_] (str "(" type " " (clojure.string/join " " (map (fn [x] (toString x)) arguments)) ")"))
+  (diff [_ name] (apply (nth (get Operations type) 1) name arguments)))
+
+(defn get-diff [name x] (diff x name))
+(defn get-diffs [name & args] (mapv (fn [x] (get-diff name x)) args))
+
 (defn diff-multiply
-  ([name a] ((get-diff name) a))
-  ([name a b] (Add (Multiply ((get-diff name) a) b) (Multiply ((get-diff name) b) a)))
-  ([name a b & xs] (diff-multiply name a (diff-multiply name b xs))))
+  ([name xs] (reduce (fn [a b] (Add (Multiply (get-diff name a) b) (Multiply (get-diff name b) a))) xs)))
 
 (defn diff-divide
-  ([name a] ((get-diff name) a))
-  ([name a b] (Divide (Subtract (Multiply ((get-diff name) a) b) (Multiply ((get-diff name) b) a)) (Multiply b b)))
-  ([name a b & xs] (diff-multiply name a (diff-multiply name b xs))))
+  ([name xs] (reduce (fn [a b] (Divide (Subtract (Multiply (get-diff name a) b) (Multiply (get-diff name b) a)) (Multiply b b))) xs)))
+
+(defn differ* [op] (fn [name & args] (op name args)))
+(defn build-operation [op type diff-f]
+  (fn [& arguments] (def Operations (assoc Operations type [op diff-f])) (constantly (Operation. type arguments))))
+
+(def Add      (build-operation +   "+"       (fn [name & args] (apply Add      (apply get-diffs name args)))))
+(def Subtract (build-operation -    "-"      (fn [name & args] (apply Subtract (apply get-diffs name args)))))
+(def Multiply (build-operation *    "*"      (differ* diff-multiply)))
+(def Divide   (build-operation div  "/"      (differ* diff-divide)))
+(def Negate   (build-operation neg  "negate" (fn [name arg]    (Negate   (get-diff name arg)))))
+(def Square   (build-operation sqr  "square" (fn [name arg]    (Multiply (get-diff name arg) TWO arg))))
+(def Sqrt     (build-operation sqrt "sqrt"   (fn [name arg]    (Divide   (Multiply arg (get-diff name arg))
+                                                                         (Multiply TWO (Sqrt (Multiply (Square arg) arg)))))))
 
 (defn parseObj [expr]
-  (let [operations {'+ Add, '- Subtract, '/ Divide, '* Multiply, 'negate Negate, 'sinh Sinh, 'cosh Cosh, 'sqrt Sqrt, 'square Square}]
+  (let [operations {'+ Add, '- Subtract, '/ Divide, '* Multiply, 'negate Negate, 'sqrt Sqrt, 'square Square}]
     (cond
       (seq? expr) (let [e (first expr)] (apply (get operations e) (map parseObj (rest expr))))
       (number? expr) (Constant expr)
